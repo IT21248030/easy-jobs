930c181379d9bf81dc03140a4d3d4296a74cb421
IT21248030 <it21248030@my.sliit.lk>
2023-03-16 22:23:03 +0530

    Initail commit

---
+++ app/src/main/java/com/example/myapplication/AllJobs.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllJobs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_jobs)
+    }
+
+
+}
